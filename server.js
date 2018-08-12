362e22e74a9fc39fe95bd1ff7c56a6ef91a0fd2e
Nigam <sanigam@paypalcorp.com>
2018-08-12 13:35:32 +0530

    checking for updated express code

--- server.js
+++ server.js
@@ -291,7 +291,9 @@ router.get('/execute-payments', function(req, res, next) {
 		  		    //custom check 
 					var webview = req.query.webview;
 					//res.redirect('/success.html?id='+body.id+"&payerId="+body.payer.payer_info.payer_id);
+					console.log('updated code is executing');
 					res.redirect('com.example.paypalcustomtabdemo:/success/'+body.id+"/"+body.payer.payer_info.payer_id);
+					console.log('updated code ended');
 					//res.writeHead(302, {'Location':"com.example.paypalcustomtabdemo:/success/"+body.id+"/"+body.payer.payer_info.payer_id });
 					//res.end();
 					//res.send(body.id);	
