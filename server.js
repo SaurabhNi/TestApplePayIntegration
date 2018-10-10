3994ebe745f4f457212428fd590d564783ca6867
Nigam <sanigam@lm-bgl-26500239.paypalcorp.com>
2018-10-10 16:33:58 +0530

    fixing for apple app site association

--- server.js
+++ server.js
@@ -355,6 +355,11 @@ router.get('/get-products', function(req, res, next) {
   	
 });
 
+router.get('/apple-app-site-association', function(req, res, next) {
+	res.send('{"applinks":{"apps":[],"details":[{"appID":"87GA28WQTJ.paypal.TestPayPalNativeXO","paths":["*"]}]}}');
+  	
+});
+
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
