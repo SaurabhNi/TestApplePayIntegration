54029c7cb43e32cd5c5e2c5ad98e6abb571d7206
Saurabh Nigam <sanigam@paypal.com>
2018-10-23 20:47:51 +0530

    changed back

--- server.js
+++ server.js
@@ -299,9 +299,9 @@ router.get('/execute-payments', function(req, res, next) {
 					console.log('updated code is executing');
 					//res.redirect('com.example.paypalcustomtabdemo://success?payID='+body.id);
 					console.log('updated code ended');
-					//res.writeHead(302, {'Location':"https://android-ec-nvp-server.herokuapp.com/paypalPaymentSuccess"});
-					//res.end();
-					window.location.href='https://android-ec-nvp-server.herokuapp.com/paypalPaymentSuccess';
+					res.writeHead(302, {'Location':"https://android-ec-nvp-server.herokuapp.com/paypalPaymentSuccess"});
+					res.end();
+					//window.location.href='https://android-ec-nvp-server.herokuapp.com/paypalPaymentSuccess';
 					//console.log('updated to old version code with latest express');
 					//res.send(body.id);	
 			  	}else {
