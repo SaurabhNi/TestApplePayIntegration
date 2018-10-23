262973540c244becd1bf942dbcd6e743ebb8c3d5
Saurabh Nigam <sanigam@paypal.com>
2018-10-23 18:29:17 +0530

    trying all the options to fix assetslink.json file

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ router.use(bodyParser.json());
 var server = http.createServer(router);
 var returnCapture = require('./returnCapture.js');
 
+
+router.use('/.well-known',express.static(path.resolve(__dirname, 'client')));
 router.use(express.static(path.resolve(__dirname, 'client')));
 router.use('/return',returnCapture);
 var messages = [];
@@ -364,9 +366,9 @@ router.get('/apple-app-site-association', function(req, res, next) {
   	
 });
 
-router.get('/.well-known/assetlinks.json', function(req, res, next) {
-	res.json('{"relation":["delegate_permission/common.handle_all_urls"],"target":{"namespace":"android_app","package_name":"com.example.paypalcustomtabdemo","sha256_cert_fingerprints":["AC:C4:A4:4E:4D:9C:01:C4:C6:6A:D6:4C:B6:6E:50:FE:8A:BC:2B:E4:C9:87:53:07:9E:CD:C0:EC:4C:14:82:B6"]}}');
-});
+//router.get('/.well-known/assetlinks.json', function(req, res, next) {
+//	res.json('{"relation":["delegate_permission/common.handle_all_urls"],"target":{"namespace":"android_app","package_name":"com.example.paypalcustomtabdemo","sha256_cert_fingerprints":["AC:C4:A4:4E:4D:9C:01:C4:C6:6A:D6:4C:B6:6E:50:FE:8A:BC:2B:E4:C9:87:53:07:9E:CD:C0:EC:4C:14:82:B6"]}}');
+//});
 
 
 router.post('/post-ipn-parameters', urlencodedParser, function(req, res, next) {
