70461072d6c7d80e5e34cc2f7e35282afbaffab9
Saurabh Nigam <sanigam@paypal.com>
2019-11-12 21:45:35 +0530

    Testing MAM UCC Vaulting

--- server.js
+++ server.js
@@ -286,19 +286,19 @@ router.post('/create-order', function(req, res, next) {
 					"payee":{
 						"merchant_id":"PPC6CRGZNWY5C"
 					},
-					"shipping":{
+					//"shipping":{
 						// "name":{
 						// 	"shipping_type":"SHIPPING"
 						// },
-						 "address":{
-							 "address_line_1":"1H Zwirkrand Wigury Str.",
-							 "address_line_2":"",
-							"admin_area_2":"Warszawa",
-						 	"admin_area_1":"",
-						 	"postal_code":"00-906",
-						 	"country_code":"PL"
-						 }
-					}
+					//	 "address":{
+					// 		 "address_line_1":"1H Zwirkrand Wigury Str.",
+					// 		 "address_line_2":"",
+					// 		"admin_area_2":"Warszawa",
+					// 	 	"admin_area_1":"",
+					// 	 	"postal_code":"00-906",
+					// 	 	"country_code":"PL"
+					// 	 }
+					// }
 
 					//"payment_instruction":{
 					//"disbursement_mode":"DELAYED"
